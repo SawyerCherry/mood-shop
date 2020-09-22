bd5fa33a2f6ee11b3207da01d17905c23fef6d60
Sawyer Cherry <sawyeracherry@gmail.com>
2020-09-22 16:47:35 -0500

    1-6 is completed

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -10,16 +10,21 @@ for (let i=0; i<data.length; ++i) {
     let img = document.createElement('img');
     let price = document.createElement('p');
     let desc = document.createElement('p');
+    let button = document.createElement('button');
     desc.innerHTML = data[i].desc
     price.innerHTML = data[i].price
+    button.innerHTML = data[i].name
     // this will change each time we go through the loop. Can you explain why?
     img.src = data[i].image
     img.width = 300
     img.height = 300
-
+    button.dataset.price = data[i].price
+    button.innerHTML = "Add to Cart"
+   
     // Add the image to the div
     newDiv.appendChild(img)
     newDiv.appendChild(desc)
     newDiv.appendChild(price)
+    newDiv.appendChild(button)
     document.querySelector('#items').appendChild(newDiv)
 }
