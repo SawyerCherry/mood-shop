01369dcfdaafaa6fd9cd47c4245c16a3c8838bfb
Sawyer Cherry <sawyeracherry@gmail.com>
2020-09-30 01:56:12 -0500

    cart fully functional

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -12,10 +12,15 @@ itemList.innerHTML = '<li> Hello World</li>'
 const cart = []
 const obj = {}
        // add Item 
+
+
+
+
 function addItem(name, price) {
     for (let i = 0; i < cart.length; i += 1) {
         if (cart[i].name === name ) {
             cart[i].qty += 1
+            showItems()
             return
         }
     }
@@ -32,7 +37,12 @@ function showItems() {
         const {name, price, qty} = cart[i]
 
         console.log(`-${cart[i].name} $${cart[i].price} x ${cart[i].qty}`)
-        itemStr += `<li>${name} $${price} x ${qty} = ${qty * price}</li>`
+        itemStr += `<li>${name} $${price} x ${qty} = ${qty * price} 
+        <button class="remove" data-name="${name}">Remove</button>
+        <button class="add-one" data-name="${name}"> + </button>
+        <button class="remove-one" data-name="${name}"> - </button>
+        <input class= "update" type="number" min="0" data-name="${name}">
+        </li>`
     }
     itemList.innerHTML = itemStr
     const total = getTotal()
@@ -68,12 +78,47 @@ function removeItem(name, qty = 0) {
             if (cart[i].qty === 0 || qty === 0 ) {
                 cart.splice(i, 1)
             }
+            showItems()
             return
         }
     }
 }
+function updateCart(name,qty) {
+    for (let i = 0; i < cart.length; i += 1) {
+        if (cart[i].name === name) {
+            if (qty< 1) {
+                removeItem(name)
+                return
+            }
+        }
+        cart[i].qty = qty
+        showItems()
+        return
+    }
+}
         // ------------------------------------------------------
+itemList.onchange = function(e) {
+    if (e.target.classList.contains('update')) {
+        const name = e.target.dataset.name
+        const qty = parseInt(e.target.value)
+        updateCart(name, qty)
+    }
+}
 
+itemList.onclick = function(e) {
+    console.log("clickedList")
+    console.log(e.target)
+    if (e.target &&e.target.classList.contains('remove')) {
+        const name = e.target.dataset.name
+        removeItem(name)
+    } else if (e.target &&e.target.classList.contains('add-one')) {
+        const name = e.target.dataset.name
+        addItem(name, 1)
+    } else if (e.target &&e.target.classList.contains('remove-one')) {
+        const name = e.target.dataset.name 
+        removeItem(name, 1)
+    }
+}
 
 for (let i=0; i<data.length; ++i) {
     // create a new div element and give it a class name
